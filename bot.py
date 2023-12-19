e3c2b09e67622c173d992e7a54152995dcdf473f
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-12-19 16:34:58 +0900

    ADD bot

---
+++ bot.py
@@ -0,0 +1,124 @@
+import asyncio
+import logging
+import os
+import tempfile
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+from typing import Any
+
+import discord
+import ffmpeg
+import tyro
+from discord.flags import Intents
+from dotenv import load_dotenv
+
+from video_capture_bot.obs_client import ObsClient
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Args:
+
+    # Message configurations
+    post_message: str = ""
+    """Message when posting to Discord."""
+    date_format: str = "%Y/%m/%d %H:%M:%S"
+    """Date string format"""
+
+    # Obs websocket configurations
+    obs_ws_host: str = "127.0.0.1"
+    """Address of obs websocker server."""
+    obs_ws_port: int = 4455
+    """Port of obs websocket server."""
+    # NOTE: Please write password to `.env` file if you set it.
+
+    # Video Configurations.
+    video_duration: float = 30  # [sec]
+    """Length of video [seconds], default is 30 secs."""
+    capture_interval: float = 60 * 30  # [sec]
+    """Interval for next capture. default is 60 mins."""
+    video_width: int = 1280
+    """Width of video."""
+    video_height: int = 720
+    """Height of video."""
+    video_bitrate: str = "1000k"
+    """Bitrate of video."""
+
+
+# def get_discord_client() -> discord.Client:
+#     intents = discord.Intents.default()
+#     intents.message_content = True
+#     return discord.Client(intents=intents)
+
+
+def convert_video(input_path: str, output_path: str, resolution: tuple[int, int], bitrate: str) -> None:
+    (
+        ffmpeg.input(input_path)
+        .output(output_path, vf=f"scale={resolution[0]}:{resolution[1]}", video_bitrate=bitrate)
+        .overwrite_output()
+        .run()
+    )
+
+
+def get_temp_video_path() -> Path:
+    temp_video_dir = Path(tempfile.TemporaryDirectory().name)
+    temp_video_dir.mkdir()
+    return temp_video_dir / "video.mp4"
+
+
+class Bot(discord.Client):
+    def __init__(self, args: Args, intents: Intents, **options: Any) -> None:
+        super().__init__(intents=intents, **options)
+
+        self.args = args
+        self.obs_client = ObsClient(
+            host=args.obs_ws_host, port=args.obs_ws_port, password=str(os.environ["OBS_WEB_SOCKET_PASSWORD"])
+        )
+        self.target_channel_id = int(os.environ["DISCORD_POST_CHANNEL_ID"])
+
+    async def on_ready(self) -> None:
+        logger.info(f"Logged on as {self.user}!")
+        self.loop.create_task(self.main_loop())
+
+    async def main_loop(self) -> None:
+        logger.info("Start main loop!")
+
+        video_path_for_post = str(get_temp_video_path())
+        channel = self.get_channel(int(os.environ["DISCORD_POST_CHANNEL_ID"]))
+
+        while True:
+            current_time = datetime.now().strftime(self.args.date_format)
+            logger.info(f"Start recording...: {current_time}")
+            video_path = await self.obs_client.capture(self.args.video_duration)
+            logger.info("End recording.")
+            message = f"{args.post_message}\nRecording start time: {current_time}"
+
+            convert_video(
+                video_path,
+                video_path_for_post,
+                (self.args.video_width, self.args.video_height),
+                self.args.video_bitrate,
+            )
+
+            await channel.send(message, file=discord.File(video_path_for_post))
+            os.remove(video_path)
+            logger.info(f"Removed: {video_path}")
+
+            if (
+                wait_time := (
+                    self.args.capture_interval - self.obs_client.wait_time_after_stop - self.args.video_duration
+                )
+            ) > 0:
+                await asyncio.sleep(wait_time)
+
+
+if __name__ == "__main__":
+    discord.utils.setup_logging()
+    args = tyro.cli(Args)
+    load_dotenv()
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = Bot(args, intents=intents)
+    bot.run(str(os.environ["DISCORD_BOT_TOKEN"]))
---
+++ video_capture_bot/obs_client.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+
+import obsws_python as obs
+from obsws_python.error import OBSSDKError, OBSSDKRequestError
+
+logger = logging.getLogger(__name__)
+
+
+class ObsClient(obs.ReqClient):
+    def __init__(self, wait_time_after_stop: float = 3.0, **kwargs):
+        super().__init__(**kwargs)
+        self.wait_time_after_stop = wait_time_after_stop
+
+    async def capture(self, duration: float) -> str:
+        """Capture video for `duration`.
+
+        Args:
+            duration: video length [seconds].
+
+        Returns:
+            captured_video_path:
+        """
+
+        self.start_record()
+        await asyncio.sleep(duration)
+        video_path = self.stop_record().output_path
+        await asyncio.sleep(self.wait_time_after_stop)
+        return video_path
+
+    def __del__(self):
+        try:
+            self.stop_record()
+        except OBSSDKError:
+            pass
+        except OBSSDKRequestError:
+            pass
